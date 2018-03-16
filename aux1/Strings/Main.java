227408a8c95544db03b5a87b017b33dbe50470a5
martinKindall <martin@rflex.cl>
2018-03-16 17:21:30 -0300

    Se reescribe la P2

---
+++ aux1/Strings/Main.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("pro");
+	}
+
+	public static boolean esRotacionCicular(String r, String w)
+	{
+		return false;
+	}
+}
--- aux1/aux1/Main.java
+++ aux1/aux1/Main.java
@@ -1,10 +1,17 @@
 package aux1;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		// main1();
+		main2();
+	}
+
+	public static void main1()
+	{
 		Fraccion prueba1 = new Fraccion(1, 2);
 		Fraccion prueba2 = new Fraccion("300/40");
 		System.out.println("probando");
@@ -25,5 +32,16 @@ public class Main {
 		}
 
 		System.out.println("La suma total es: "+total);
+	}
+
+	public static void main2()
+	{
+		// crear funcion que cree stream de inputs
+		// 
+		// usar un reduce de streams que transforme cada elemento
+		// del stream a una fraccion y las sume
+		// 
+		// evaluar el stream en el numero de fracciones elegido por el usuario
+		// y hacerle reduce?
 	}
 }
