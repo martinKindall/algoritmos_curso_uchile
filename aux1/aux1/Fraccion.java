e9cf73a6c4d540a8abb194f5387be14e81a23011
martinKindall <martin@rflex.cl>
2018-03-16 15:04:55 -0300

    P2 se crean mas metodos Fraccion

--- aux1/aux1/Fraccion.java
+++ aux1/aux1/Fraccion.java
@@ -15,4 +15,31 @@ public class Fraccion {
 		this.num = Integer.parseInt(partes[0]);
 		this.den = Integer.parseInt(partes[1]);
 	}
+
+	public static int mcd(int a, int b)
+	{
+		if (b == 0)
+		{
+			return a;
+		}
+		return mcd(b, a%b);
+	}
+
+	public void simplificar()
+	{
+		int mcd = mcd(this.num, this.den);
+		this.num /= mcd;
+		this.den /= mcd;
+	}
+
+	public Fraccion suma(Fraccion other)
+	{
+		int num = other.num * this.den + this.num * other.den;
+		int den = other.den * this.den;
+
+		Fraccion result = new Fraccion(num, den);
+		result.simplificar();
+
+		return result;
+	}
 }
