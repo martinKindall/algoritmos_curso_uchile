eaa7c021f58b00fe3b35ac3a3d5a520f7cef32b1
martinKindall <martin@rflex.cl>
2018-03-16 14:44:53 -0300

    empieza la auxiliar 1 P2

---
+++ aux1/aux1/Fraccion.java
@@ -0,0 +1,11 @@
+package aux1;
+
+public class Fraccion {
+	public int num, den;
+
+	public Fraccion(int num, int den)
+	{
+		this.num = num;
+		this.den = den;
+	}
+}
---
+++ aux1/aux1/Main.java
@@ -0,0 +1,9 @@
+package aux1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Fraccion prueba = new Fraccion(1, 2);
+		System.out.println("probando");
+	}
+}
