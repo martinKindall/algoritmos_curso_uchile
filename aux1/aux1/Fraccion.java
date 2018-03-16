7ff29ba0bee18785e475a48eab705cd560622c08
martinKindall <martin@rflex.cl>
2018-03-16 15:12:37 -0300

    Se crea scanner para leer los datos de Main

--- aux1/aux1/Fraccion.java
+++ aux1/aux1/Fraccion.java
@@ -42,4 +42,9 @@ public class Fraccion {
 
 		return result;
 	}
+
+	public String toString()
+	{
+		return this.num + "/" + this.den;
+	}
 }
--- aux1/aux1/Main.java
+++ aux1/aux1/Main.java
@@ -1,10 +1,19 @@
 package aux1;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Fraccion prueba1 = new Fraccion(1, 2);
 		Fraccion prueba2 = new Fraccion("300/40");
 		System.out.println("probando");
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("n?");
+		int nroFracciones = scanner.nextInt();
+
+		System.out.println("Fraccion 1?");
 	}
 }
