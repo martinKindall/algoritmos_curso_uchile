8cd79a5ac80794a1efe480933b76849a1dda9b82
martinKindall <martin@rflex.cl>
2018-03-16 21:11:20 -0300

    P3 a) finalizada con 2 formas

--- aux1/Strings/Main.java
+++ aux1/Strings/Main.java
@@ -3,11 +3,37 @@ package Strings;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("pro");
+		String word_1 = "algoritmo";
+		String word_2 = "ritmoalgo";
+		String word_3 = "ritmoalga";
+
+		System.out.println(esRotacionCicular(word_1, word_2));
+		System.out.println(esRotacionCicular(word_1, word_3));
+
+		System.out.println(esRotacionCicular_v2(word_1, word_2));
+		System.out.println(esRotacionCicular_v2(word_1, word_3));
 	}
 
 	public static boolean esRotacionCicular(String r, String w)
 	{
+		if (r.equals(w)) return true;
+
+		for(int i=0; i<r.length(); ++i)
+		{
+			r = r.substring(1, r.length()) + r.substring(0,1);
+
+			System.out.println(r);
+
+			if (r.equals(w)) return true;
+		}
+
 		return false;
+	}
+
+	public static boolean esRotacionCicular_v2(String r, String w)
+	{
+		if (r.equals(w)) return true;
+
+		return (r+r).contains(w);
 	}
 }
