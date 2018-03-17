b398b22f37d4c283e4d382c63162849de33a07a4
martinKindall <martin@rflex.cl>
2018-03-16 21:46:23 -0300

    P3 b) listo

--- aux1/Strings/Main.java
+++ aux1/Strings/Main.java
@@ -1,17 +1,14 @@
 package Strings;
 
+import java.util.stream.Stream;
+
 public class Main {
 
 	public static void main(String[] args) {
-		String word_1 = "algoritmo";
-		String word_2 = "ritmoalgo";
-		String word_3 = "ritmoalga";
-
-		System.out.println(esRotacionCicular(word_1, word_2));
-		System.out.println(esRotacionCicular(word_1, word_3));
-
-		System.out.println(esRotacionCicular_v2(word_1, word_2));
-		System.out.println(esRotacionCicular_v2(word_1, word_3));
+		// testRotacionCircular();
+		
+		testInvertirPalabra();
+		testEsPalindrome();
 	}
 
 	public static boolean esRotacionCicular(String r, String w)
@@ -35,5 +32,53 @@ public class Main {
 		if (r.equals(w)) return true;
 
 		return (r+r).contains(w);
+	}
+
+	public static boolean palabraEsPalindrome(String s)
+	{
+		return s.equals(invertirPalabra(s));
+	}
+
+	public static String invertirPalabra(String s)
+	{
+		String invertida = new StringBuilder(s).reverse().toString();
+		return invertida;
+	}
+
+	public static void testRotacionCircular()
+	{
+		String word_1 = "algoritmo";
+		String word_2 = "ritmoalgo";
+		String word_3 = "ritmoalga";
+
+		System.out.println(esRotacionCicular(word_1, word_2));
+		System.out.println(esRotacionCicular(word_1, word_3));
+
+		System.out.println(esRotacionCicular_v2(word_1, word_2));
+		System.out.println(esRotacionCicular_v2(word_1, word_3));
+	}
+
+	public static void testEsPalindrome()
+	{
+		String palindrome_1 = "aviva";
+		String palindrome_2 = "aboba";
+		String palindrome_3 = "reconocer";
+		String palindrome_4 = "reconocera";
+
+		System.out.println(palabraEsPalindrome(palindrome_1));
+		System.out.println(palabraEsPalindrome(palindrome_2));
+		System.out.println(palabraEsPalindrome(palindrome_3));
+		System.out.println(palabraEsPalindrome(palindrome_4));
+	}
+
+	public static void testInvertirPalabra()
+	{
+		String word = "elefante";
+		String inverted = "etnafele";
+
+		if (!inverted.equals(invertirPalabra(word)))
+		{
+			System.out.println("error al invertir palabra");
+		}
 	}
 }
