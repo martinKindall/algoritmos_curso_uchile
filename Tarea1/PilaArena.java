935583a4a1ed616b1383571096e917bcf5726737
martinKindall <martin@rflex.cl>
2018-03-20 23:02:04 -0300

    probando la clase Ventana, el tamaño de la ventana es en pixeles

--- Tarea1/PilaArena.java
+++ Tarea1/PilaArena.java
@@ -7,6 +7,17 @@ public class PilaArena {
 		
 		System.out.println("Ingrese N: ");
 		int nroFrac = scanner.nextInt();
+
+		Ventana ventana = new Ventana(800, "Primera ventana");
+		int[][] tablero = new int[nroFrac][nroFrac];
+
+		tablero[1][1] = 3;
+		tablero[0][1] = 2;
+		tablero[1][0] = 2;
+		tablero[2][2] = 1;
+		tablero[2][1] = 0;
+
+		ventana.mostrarMatriz(tablero);
 	}
 
 }
