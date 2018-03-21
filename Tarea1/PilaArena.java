f089e8ffd757dda4cb9d6b9ffed307320dc54e89
martinKindall <martin@rflex.cl>
2018-03-20 23:21:22 -0300

    funciones para capturar input de usuario y estimar el lado del tablero

--- Tarea1/PilaArena.java
+++ Tarea1/PilaArena.java
@@ -3,13 +3,30 @@ import java.util.Scanner;
 public class PilaArena {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("Ingrese N: ");
-		int nroFrac = scanner.nextInt();
+		int nGranos = ingresarN();
+		int ladoTablero = estimarTableroSegunNGranos(nGranos);
 
+		// test(ladoTablero);
+		
+		Ventana ventana = new Ventana(800, "Pila de arena");
+
+		int[][] tablero = new int[ladoTablero][ladoTablero];
+	}
+
+	public static int ingresarN()
+	{
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese N: ");
+		int granos = scanner.nextInt();
+
+		return granos;
+	}
+
+	public static void test(int ladoTablero)
+	{
 		Ventana ventana = new Ventana(800, "Primera ventana");
-		int[][] tablero = new int[nroFrac][nroFrac];
+		int[][] tablero = new int[ladoTablero][ladoTablero];
 
 		tablero[1][1] = 3;
 		tablero[0][1] = 2;
@@ -20,4 +37,15 @@ public class PilaArena {
 		ventana.mostrarMatriz(tablero);
 	}
 
+	/**
+	 * [estimarTableroSegunNGranos: estima el lado del tablero cuadrado para que quepan todos los granos de arena]
+	 * @param  granos [int]
+	 * @return        [int]
+	 */
+	public static int estimarTableroSegunNGranos(int granos)
+	{
+		int lado = (int)Math.sqrt(granos);
+
+		return lado % 2 == 0 ? lado+1 : lado;
+	}
 }
