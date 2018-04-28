96a445266e6e8770c246d54cd55fa0b1ef24f990
martinKindall <martin@rflex.cl>
2018-04-28 13:47:29 -0300

    Se termina el codigo de la tarea con tests incluidos

--- Tarea2/Matrices.java
+++ Tarea2/Matrices.java
@@ -2,7 +2,38 @@ import java.util.Scanner;
 
 public class Matrices{
 
-	public static int multiplicarMatrices(int[] p, int[][] m, int[][] s)
+	static int MAX_SIZE = 100;
+	static String EOF = "EOF";
+
+	public static void main(String[] args) 
+	{
+		tests();
+		
+		String[] inputs = matrixParser(MAX_SIZE);
+		// printStringArray(inputs);
+
+		for (int i=0; i<inputs.length; i++) 
+		{
+			System.out.println(stringMultiplicarMatrices(inputs[i]));
+		}
+	}
+
+	static String stringMultiplicarMatrices(String s)
+	{
+		int[] matrices = strToIntArr(s);
+		int nMatrices = matrices.length;
+
+		int[][] steps = new int[nMatrices][nMatrices];
+		int[][] costos = new int[nMatrices][nMatrices];
+
+		multiplicarMatrices(matrices, costos, steps);
+		// printMatrix(steps);
+		// printMatrix(costos);
+
+		return printParentesis(steps, 1, nMatrices-1);
+	}
+
+	static int multiplicarMatrices(int[] p, int[][] m, int[][] s)
 	{
 		int nMatrices = p.length;
 		int n = nMatrices - 1;
@@ -37,7 +68,6 @@ public class Matrices{
 	{
 		for(int i = 0; i < palabras.length; i++)
 		{
-			if (palabras[i] == null) break;
 			System.out.println(palabras[i]);
 		}
 	}
@@ -57,6 +87,21 @@ public class Matrices{
 		}
 	}
 
+	static int[] strToIntArr(String s)
+	{
+		String[] strMatrices = s.split(" ");
+		int nMatrices = strMatrices.length;
+
+		int[] matrices = new int[nMatrices];
+
+		for (int i=0; i<nMatrices; i++) 
+		{
+			matrices[i] = Integer.parseInt(strMatrices[i]);
+		}
+
+		return matrices;
+	}
+
 	static String printParentesis(int[][] s, int i, int j)
 	{
 		if (i == j)
@@ -71,51 +116,43 @@ public class Matrices{
 				printParentesis(s, s[i][j] + 1, j) +
 				")";
 		}
-	}
+	}	
 
-	public static void main(String[] args) 
+	static String[] matrixParser(int maxSize)
 	{
-		// tests();
-		
-		String[] inputs = new String[10];
-		matrixParser(inputs);
-	}
+		String[] inputs = new String[maxSize];
 
-	static void matrixParser(String[] inputs)
-	{
 		Scanner scanner = new Scanner(System.in);
 
 	    String line = scanner.nextLine(); 
 	    int index = -1;
 
-		while (line.compareTo("") != 0) 
+		while (line.compareTo(EOF) != 0 && index < maxSize) 
 		{
 			++index;
 			inputs[index] = line;
 		    line = scanner.nextLine(); 
 		}
 
-		printStringArray(inputs);
+		if (index < maxSize - 1)
+		{
+			String[] resize = new String[index+1];
+
+			for (int i=0; i<=index; i++) 
+			{
+				resize[i] = inputs[i];
+			}
+
+			return resize;
+		}
+
+		return inputs;
 	}
 
 	static void tests()
 	{
-		int[] matrices = {100, 10, 100, 10};
-		int nMatrices = matrices.length;
-
-		int[][] steps = new int[nMatrices][nMatrices];
-		int[][] costos = new int[nMatrices][nMatrices];
-
-		int costo = multiplicarMatrices(matrices, costos, steps);
-
-		System.out.println("El costo de multiplicar es: " + costo);
-
-		System.out.println("La matriz de pasos es: ");
-		printMatrix(steps);
-
-		System.out.println("La matriz de costos es: ");
-		printMatrix(costos);
-
-		System.out.println(printParentesis(steps, 1, nMatrices-1));
+		assert(stringMultiplicarMatrices("10 100 10 100").compareTo("((..).)") == 0);
+		assert(stringMultiplicarMatrices("2 3 5 4").compareTo("((..).)") == 0);
+		assert(stringMultiplicarMatrices("2 3 5 4 1 2").compareTo("((.(.(..))).)") == 0);
 	}
 }
