89ff68906a9f70853c8559c96f6c1a94e414f2a0
martinKindall <martin@rflex.cl>
2018-04-28 11:31:33 -0300

    Se empieza Tarea 2

---
+++ Tarea2/Matrices.java
@@ -0,0 +1,67 @@
+public class Matrices{
+
+	public static void main(String[] args) 
+	{
+		int[] matrices = {100, 10, 100, 10};
+		int nMatrices = matrices.length;
+
+		int[][] steps = new int[nMatrices][nMatrices];
+		int[][] costos = new int[nMatrices][nMatrices];
+
+		int costo = multiplicarMatrices(matrices, costos, steps);
+
+		System.out.println("El costo de multiplicar es: " + costo);
+
+		System.out.println("La matriz de pasos es: ");
+		printMatrix(steps);
+
+		System.out.println("La matriz de costos es: ");
+		printMatrix(costos);
+	}
+
+	static int multiplicarMatrices(int[] p, int[][] m, int[][] s)
+	{
+		int nMatrices = p.length;
+		int n = nMatrices - 1;
+
+		for (int i=1; i <= n; i++) 
+		{
+			m[i][i] = 0;
+		}
+
+		for (int l=2; l<=n; l++) 
+		{
+			for (int i=1; i<=n-l+1; i++) 
+			{
+				int j = i + l - 1;
+				m[i][j] = Integer.MAX_VALUE;
+				for (int k=i; k<=j-1; k++) 
+				{
+					int q = m[i][k] + m[k+1][j] + p[i-1]*p[k]*p[j];
+					if(q < m[i][j])
+					{
+						m[i][j] = q;
+						s[i][j] = k;
+					}
+				}
+			}	
+		}
+
+		return m[1][n];
+	}
+
+	public static void printMatrix(int[][] mat)
+	{
+		System.out.println("Matriz = \n");
+		for(int i = 0; i < mat.length; i++)
+		{
+			System.out.print("|");
+			for(int j = 0; j < mat.length; j++)
+			{
+				System.out.print(mat[i][j]);
+				System.out.print("|");
+			}
+			System.out.print("\n");
+		}
+	}
+}
