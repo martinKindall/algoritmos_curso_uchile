38aaebd423571a5586c00521ef136ac684d0b332
martinKindall <martin@rflex.cl>
2018-05-09 14:48:28 -0300

    Tarea3: comienza tarea 3

---
+++ Tarea3/Derivar_polaca.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Derivar_polaca{
+
+	public static void main(String[] args) 
+	{
+		tests();
+		
+		System.out.print("Ingrese expresion en notacion polaca inversa: ");
+		Scanner scanner = new Scanner(System.in);
+
+	    String line = scanner.nextLine();
+
+	    System.out.println(line);
+	}
+
+	static void tests()
+	{
+
+	}
+}
