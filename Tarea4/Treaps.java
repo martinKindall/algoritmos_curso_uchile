c1c9883656d4da9be47fedf39a38b55018c4ca62
martinKindall <martin@rflex.cl>
2018-06-14 10:38:49 -0400

    Empieza T4

---
+++ Tarea4/Treaps.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Treaps{
+
+	public static void main(String[] args) 
+	{
+		System.out.print("Ingrese par ordenado: ");
+		Scanner scanner = new Scanner(System.in);
+	    String expresion = scanner.nextLine();
+
+	    String[] parOrd = expresion.split(" ");
+		int numX = Integer.parseInt(parOrd[0]);
+		double numY = Double.parseDouble(parOrd[1]);
+
+		System.out.println(numX);
+		System.out.println(numY);
+	}
+}
